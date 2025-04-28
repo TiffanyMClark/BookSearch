b96a2d87d65f1631a898a48818e4267089149aca
tiffany clark <tiffanymarie9811@gmail.com>
2025-04-28 19:43:51 -0400

    updated vite file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "node:path";
 import type { Request, Response } from "express";
-import db from "./config/connection.js";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { typeDefs, resolvers } from "./schemas/index.js";
